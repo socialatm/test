a0430f91bd72cc200165f10d3825a88efca53291
Mario <mario@mariovavti.com>
2022-09-30 10:25:46 +0000

    fix owa in case of hub reinstalls

--- Zotlabs/Module/Owa.php
+++ Zotlabs/Module/Owa.php
@@ -32,14 +32,14 @@ class Owa extends Controller {
 				$keyId = $sigblock['keyId'];
 				if ($keyId) {
 					$r = q("SELECT * FROM hubloc LEFT JOIN xchan ON hubloc_hash = xchan_hash
-						WHERE hubloc_id_url = '%s' AND xchan_pubkey != '' ",
+						WHERE hubloc_id_url = '%s' AND hubloc_deleted = 0 ORDER BY hubloc_id DESC",
 						dbesc($keyId)
 					);
 					if (! $r) {
-						$found = discover_by_webbie(str_replace('acct:','',$keyId));
+						$found = discover_by_webbie($keyId);
 						if ($found) {
 							$r = q("SELECT * FROM hubloc LEFT JOIN xchan ON hubloc_hash = xchan_hash
-								WHERE hubloc_id_url = '%s' AND xchan_pubkey != '' ",
+								WHERE hubloc_id_url = '%s' AND hubloc_deleted = 0 ORDER BY hubloc_id DESC ",
 								dbesc($keyId)
 							);
 						}
@@ -61,10 +61,43 @@ class Owa extends Controller {
 								logger('OWA fail: ' . $hubloc['hubloc_id'] . ' ' . $hubloc['hubloc_id_url']);
 							}
 						}
+
+						if (!$ret['success']) {
+
+							// Possible a reinstall?
+							// In this case we probably already have an old hubloc
+							// but not the new one yet.
+
+							$found = discover_by_webbie($keyId);
+
+							if ($found) {
+								$r = q("SELECT * FROM hubloc LEFT JOIN xchan ON hubloc_hash = xchan_hash
+									WHERE hubloc_id_url = '%s' AND hubloc_deleted = 0 ORDER BY hubloc_id DESC LIMIT 1",
+									dbesc($keyId)
+								);
+
+								if ($r) {
+									$verified = HTTPSig::verify(file_get_contents('php://input'), $r[0]['xchan_pubkey']);
+									if ($verified && $verified['header_signed'] && $verified['header_valid'] && ($verified['content_valid'] || (! $verified['content_signed']))) {
+										logger('OWA header: ' . print_r($verified,true), LOGGER_DATA);
+										logger('OWA success: ' . $r[0]['hubloc_id_url'], LOGGER_DATA);
+										$ret['success'] = true;
+										$token = random_string(32);
+										Verify::create('owt', 0, $token, $r[0]['hubloc_id_url']);
+										$result = '';
+										openssl_public_encrypt($token, $result, $r[0]['xchan_pubkey']);
+										$ret['encrypted_token'] = base64url_encode($result);
+									} else {
+										logger('OWA fail: ' . $hubloc['hubloc_id'] . ' ' . $hubloc['hubloc_id_url']);
+									}
+								}
+							}
+						}
 					}
 				}
 			}
 		}
+
 		json_return_and_die($ret,'application/x-zot+json');
 	}
 }
