f70956964be5792be296a4e3ec1889a4be2fd475
Mario <mario@mariovavti.com>
2022-10-14 15:23:23 +0000

    fix php warning

--- Zotlabs/Lib/Libzot.php
+++ Zotlabs/Lib/Libzot.php
@@ -2007,7 +2007,7 @@ class Libzot {
 				}
 			}
 
-			if ($AS->obj['actor'] && $AS->obj['actor']['id'] && $AS->obj['actor']['id'] !== $AS->actor['id']) {
+			if (isset($AS->obj['actor']['id']) && $AS->obj['actor']['id'] !== $AS->actor['id']) {
 				$y = import_author_xchan(['url' => $AS->obj['actor']['id']]);
 				if (!$y) {
 					logger('FOF Activity: no object actor');
