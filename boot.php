b17a0aef17f858bddff1798ac8622c82d3b7a26b
Mario <mario@mariovavti.com>
2022-11-14 05:47:16 +0000

    fix typo and version 7.8.6

--- boot.php
+++ boot.php
@@ -1,4 +1,4 @@
-s<?php
+<?php
 /**
  * @file boot.php
  *
@@ -60,7 +60,7 @@ require_once('include/bbcode.php');
 require_once('include/items.php');
 
 define('PLATFORM_NAME', 'hubzilla');
-define('STD_VERSION', '7.8.5');
+define('STD_VERSION', '7.8.6');
 define('ZOT_REVISION', '6.0');
 
 define('DB_UPDATE_VERSION', 1253);
