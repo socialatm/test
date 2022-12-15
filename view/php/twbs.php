aee35e22e7e2b2841b4ded87e411e3eedde49c81
Ray <raypeaslee@gmail.com>
2022-12-15 16:52:48 -0500

    Update twbs.php
    getting there

--- view/php/twbs.php
+++ view/php/twbs.php
@@ -75,13 +75,13 @@
     <p>Get three columns <strong>starting at desktops and scaling to large desktops</strong> of various widths. Remember, grid columns should add up to twelve for a single horizontal block. More than that, and columns start stacking no matter the viewport.</p>
     <div class="row gx-2">
       <div class="col-md-3">
-        <div class="p-3 border bg-light ">.col-md-3</div>
+        <div class="p-2 border bg-light ">.col-md-3</div>
       </div>
       <div class="col-md-6">
-        <div class="p-3 border bg-light ">.col-md-6</div>
+        <div class="p-2 border bg-light ">.col-md-6</div>
       </div>
       <div class="col-md-3">
-        <div class=" p-3 border bg-light">.col-md-3</div>
+        <div class=" p-2 border bg-light">.col-md-3</div>
       </div>
     </div>
 
