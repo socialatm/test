34d466438d76ccdae38f101893d8954f56db037d
Ray <raypeaslee@gmail.com>
2022-12-15 15:53:06 -0500

    Update twbs.php
    playing with gutters

--- view/php/twbs.php
+++ view/php/twbs.php
@@ -73,7 +73,7 @@
 
     <h2 class="mt-4">Three unequal columns</h2>
     <p>Get three columns <strong>starting at desktops and scaling to large desktops</strong> of various widths. Remember, grid columns should add up to twelve for a single horizontal block. More than that, and columns start stacking no matter the viewport.</p>
-    <div class="row mb-3 text-center">
+    <div class="row g-2">
       <div class="col-md-3 themed-grid-col">.col-md-3</div>
       <div class="col-md-6 themed-grid-col">.col-md-6</div>
       <div class="col-md-3 themed-grid-col">.col-md-3</div>
