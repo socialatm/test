0ec3d3fdc361bd1620326cffe29f5f29f77d998c
Ray <raypeaslee@gmail.com>
2022-12-15 16:08:34 -0500

    Update twbs.php
    more of the same

--- view/php/twbs.php
+++ view/php/twbs.php
@@ -73,7 +73,7 @@
 
     <h2 class="mt-4">Three unequal columns</h2>
     <p>Get three columns <strong>starting at desktops and scaling to large desktops</strong> of various widths. Remember, grid columns should add up to twelve for a single horizontal block. More than that, and columns start stacking no matter the viewport.</p>
-    <div class="row g-2">
+    <div class="row row-cols-2 ">
       <div class="col-md-3 p-3 border bg-light ">.col-md-3</div>
       <div class="col-md-6 p-3 border bg-light ">.col-md-6</div>
       <div class="col-md-3 p-3 border bg-light">.col-md-3</div>
