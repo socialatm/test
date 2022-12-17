36782ee8e46fb4514c9fc13888647a886ff57458
Ray <raypeaslee@gmail.com>
2022-12-16 21:30:08 -0500

    new default template
    responsive twitter bootstrap template is the new default

--- view/php/default.php
+++ view/php/default.php
@@ -1,38 +1,87 @@
-<?php
-/**
- *   * Name: default
- *   * Description: Hubzilla default 3-column layout
- *   * Version: 1
- *   * Author: Mario Vavti
- *   * Maintainer: Mario Vavti
- *   * ContentRegion: aside, left_aside_wrapper
- *   * ContentRegion: content, region_2
- *   * ContentRegion: right_aside, right_aside_wrapper
- */
-?>
-<!DOCTYPE html >
-<html prefix="og: http://ogp.me/ns#">
-<head>
-  <title><?php if(x($page,'title')) echo $page['title'] ?></title>
-  <script>var baseurl="<?php echo z_root() ?>";</script>
-  <?php if(x($page,'htmlhead')) echo $page['htmlhead'] ?>
-</head>
-<body <?php if($page['direction']) echo 'dir="rtl"' ?> >
-	<?php if(x($page,'banner')) echo $page['banner']; ?>
-	<header><?php if(x($page,'header')) echo $page['header']; ?></header>
-	<?php if(x($page,'nav')) echo $page['nav']; ?>
-	<main>
-		<div class="content">
-			<div class="columns">
-				<aside id="region_1"><div class="aside_spacer_top_left"></div><div class="aside_spacer_left"><div id="left_aside_wrapper" class="aside_wrapper"><?php if(x($page,'aside')) echo $page['aside']; ?></div></div></aside>
-				<section id="region_2"><?php if(x($page,'content')) echo $page['content']; ?>
-					<div id="page-footer"></div>
-					<div id="pause"></div>
-				</section>
-				<aside id="region_3" class="d-none d-xl-block"><div class="aside_spacer_top_right"></div><div class="aside_spacer_right"><div id="right_aside_wrapper" class="aside_wrapper"><?php if(x($page,'right_aside')) echo $page['right_aside']; ?></div></div></aside>
-			</div>
-		</div>
-	</main>
-	<footer><?php if(x($page,'footer')) echo $page['footer']; ?></footer>
-</body>
+<!doctype html>
+<html lang="en" prefix="og: http://ogp.me/ns#">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title><?php if(x($page,'title')) echo $page['title'] ?></title>
+    <script>var baseurl="<?php echo z_root() ?>";</script>
+    <?php if(x($page,'htmlhead')) echo $page['htmlhead'] ?>
+
+    <link href="./twbs_assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
+    
+  </head>
+  <body class="py-4" <?php if($page['direction']) echo 'dir="rtl"' ?> >
+      <?php if(x($page,'nav')) echo $page['nav']; ?>
+      <?php if(x($page,'banner')) echo $page['banner']; ?>
+	    <header><?php if(x($page,'header')) echo $page['header']; ?></header>
+	  <main class="mt-5">
+      <div class="container">
+        <div class="row gx-2">
+          <div class="col-md-3">
+            <div id="region_1" class="p-2 border bg-light "><?php if(x($page,'aside')) echo $page['aside']; ?></div>
+          </div>
+          <div id="region_2" class="col-md-6">
+            <div class="p-2 border bg-light "><?php if(x($page,'content')) echo $page['content']; ?></div>
+          </div>
+          <div class="col-md-3">
+            <div id="region_3" class=" p-2 border bg-light"><?php if(x($page,'right_aside')) echo $page['right_aside']; ?></div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
 </html>
