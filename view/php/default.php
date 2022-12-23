639225027f713216f8bc79f592409c2c4d74549b
Ray <raypeaslee@gmail.com>
2022-12-23 10:19:04 -0500

    Update default.php

--- view/php/default.php
+++ view/php/default.php
@@ -28,7 +28,7 @@
       <!-- end row -->
       <!-- start footer -->
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-4">
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <p class="col-md-4 mb-0 text-muted">&copy; 2022 Company, Inc</p>
 
