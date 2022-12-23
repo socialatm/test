48e0d76338d441994a5346d050e3dff6edd5be36
Ray <raypeaslee@gmail.com>
2022-12-23 10:24:32 -0500

    Update default.php

--- view/php/default.php
+++ view/php/default.php
@@ -26,11 +26,16 @@
           </div>
       </div>
       <!-- end row -->
-      <!-- start footer -->
-      <div class="row">
-        <div class="col-md-4">
+    </div>
+    <!-- end container -->
+    <!-- start footer -->
+    <div class="container">
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <p class="col-md-4 mb-0 text-muted">&copy; 2022 Company, Inc</p>
+
+    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+    </a>
 
     <ul class="nav col-md-4 justify-content-end">
       <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
@@ -41,9 +46,7 @@
     </ul>
   </footer>
 </div>
-</div>
-      <!-- end footer -->
-    </div>
-    <!-- end container -->
+
+    <!-- end footer -->
   </body>
 </html>
