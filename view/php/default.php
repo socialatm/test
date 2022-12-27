a29f054666cb0cf33e253099e548621407199ed5
Ray <raypeaslee@gmail.com>
2022-12-27 11:18:24 -0500

    Update default.php
    still margins

--- view/php/default.php
+++ view/php/default.php
@@ -11,7 +11,7 @@
   <body class="py-4 mt-5 bg-light" <?php if($page['direction']) echo 'dir="rtl"' ?> >
       <?php if(x($page,'nav')) echo $page['nav']; ?>
       <?php if(x($page,'banner')) echo $page['banner']; ?>
-	    <header><?php if(x($page,'header')) echo $page['header']; ?></header>
+	    <?php if(x($page,'header')) echo $page['header']; ?>
 	  <div class="container-fluid">
 
       <!-- start row -->
