df89da030ea0fa6410d932ad97643493d0748bc3
Ray <raypeaslee@gmail.com>
2023-01-02 07:31:48 -0500

    Update default.php
    remove banner area from default template
    fixes https://github.com/socialatm/test/issues/22

--- view/php/default.php
+++ view/php/default.php
@@ -10,8 +10,7 @@
   </head>
   <body class="py-4 mt-5 bg-light" <?php if($page['direction']) echo 'dir="rtl"' ?> >
       <?php if(x($page,'nav')) echo $page['nav']; ?>
-      <?php if(x($page,'banner')) echo $page['banner']; ?>
-	  <header class="container-fluid mb-3"><?php if(x($page,'header')) echo $page['header']; ?></header>
+    <header class="container-fluid mb-3"><?php if(x($page,'header')) echo $page['header']; ?></header>
 	  <div class="container-fluid">
 
       <!-- start row -->
