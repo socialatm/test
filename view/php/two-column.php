d155275c15ff7a119d4dbd44e8543184d2dbc7d0
Ray <raypeaslee@gmail.com>
2023-01-02 13:05:47 -0500

    Update two-column.php

--- view/php/two-column.php
+++ view/php/two-column.php
@@ -10,34 +10,26 @@
   </head>
   <body class="py-4 mt-5 bg-light" <?php if($page['direction']) echo 'dir="rtl"' ?> >
       <?php if(x($page,'nav')) echo $page['nav']; ?>
-  <!--  <header class="container-fluid mb-3"><?php if(x($page,'header')) echo $page['header']; ?>two-column</two-column></header> -->
-	  <div class="container-fluid">
-
+    <div class="container-fluid">
       <!-- start row -->
       <div class="row gx-3 bg-light mb-3" >
+
+<!--
         <div class="col-md-3">
           <div id="region_1" class="p-3 border border-primary rounded bg-white"><?php if(x($page,'aside')) echo $page['aside']; ?></div>
         </div>
+-->
+
         <div id="region_2" class="col-md-6">
           <div class="p-3 border border-primary rounded bg-white">
             <?php if(x($page,'content')) echo $page['content']; ?>
           </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-6">
           <div id="region_3" class=" p-3 border border-primary rounded bg-white"><?php if(x($page,'right_aside')) echo $page['right_aside']; ?></div>
         </div>
       </div>
       <!-- end row -->
-
-      <!-- start footer 
-      <div class="row gx-3 bg-light mb-3" >
-        <div id="region_5" class="col-md-12">
-          <div class="p-3 border border-primary rounded bg-white">
-       {{*     <?php if(x($page,'footer')) echo $page['footer']; ?> *}}
-          </div>
-        </div>
-      </div>
-      -- end footer -->
     </div>
     <!-- end container -->
   </body>
