10c993e7a3f4225e58d2589af28d6606108a5f08
Ray <raypeaslee@gmail.com>
2023-01-04 19:56:49 -0500

    Update redbasic.js
    remove old swipe function

--- view/theme/redbasic/js/redbasic.js
+++ view/theme/redbasic/js/redbasic.js
@@ -25,48 +25,6 @@ $(document).ready(function() {
 	setInterval(function () {checkNotify();}, 10 * 1000);
 	/* end notifications code */
 
-	var touch_start = null;
-	var touch_max = window.innerWidth / 10;
-
-	window.addEventListener('touchstart', function(e) {
-		if (e.touches.length === 1){
-			//just one finger touched
-			touch_start = e.touches.item(0).clientX;
-			if (touch_start < touch_max) {
-				$('html, body').css('overflow-y', 'hidden');
-			}
-		}
-		else {
-			//a second finger hit the screen, abort the touch
-			touch_start = null;
-		}
-	});
-
-	window.addEventListener('touchend', function(e) {
-		$('html, body').css('overflow-y', '');
-
-		let touch_offset = 30; //at least 30px are a swipe
-		if (touch_start) {
-			//the only finger that hit the screen left it
-			let touch_end = e.changedTouches.item(0).clientX;
-
-			if (touch_end > (touch_start + touch_offset)) {
-				//a left -> right swipe
-				if (touch_start < touch_max) {
-					toggleAside('right');
-				}
-			}
-			if (touch_end < (touch_start - touch_offset)) {
-				//a right -> left swipe
-				//toggleAside('left');
-			}
-		}
-	});
-
-	$(document).on('hz:hqControlsClickAction', function(e) {
-		toggleAside('left');
-	});
-
 });
 
 function setStyle(element, cssProperty) {
