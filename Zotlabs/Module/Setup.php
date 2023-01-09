f04b98bbdcfae94d9ac61d2c0a84e743f6a570af
Ray <raypeaslee@gmail.com>
2023-01-09 15:28:59 -0500

    Update Setup.php
    another attempt

--- Zotlabs/Module/Setup.php
+++ Zotlabs/Module/Setup.php
@@ -767,6 +767,7 @@ class Setup extends \Zotlabs\Web\Controller {
 
 		$baseurl = z_root();
 		require_once('Zotlabs/Module/Register.php');
+		get();
 		return
 			t('<h1>What next?</h1>')
 			."<div class=\"alert alert-info\">".t('IMPORTANT: You will need to [manually] setup a scheduled task for the poller.').EOL
